80de53ea3d1a88f9cbc55dff2e3b662f5483e20f
Alex Howard <alexamh@zoho.com>
2023-05-14 22:33:07 -0400

    Added tests for TicTacToe

--- src/test/java/TestTicTacToeGame.java
+++ src/test/java/TestTicTacToeGame.java
@@ -3,23 +3,221 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.alexhoward.tictactoe.TicTacToeGame;
 
 public class TestTicTacToeGame {
-    TicTacToeGame game = new TicTacToeGame();
+  private TicTacToeGame game;
 
-    @BeforeAll
-    public static void setUp() {
-      System.out.println("BeforeClass");
-    }
+  @BeforeEach
+  public void setUp() {
+    game = new TicTacToeGame();
+  }
 
-    @AfterAll
-    public static void tearDown() {
-      System.out.println("AfterClass");
+    @Test
+    public void testGameCreation() {
+      assertEquals(1, game.getCurrentPlayer());
+      assertEquals(0, game.getGameGrid()[0][0]);
+      assertEquals(0, game.getGameGrid()[1][1]);
+      assertEquals(0, game.getGameGrid()[2][2]);
+      assertEquals(false, game.getGameOver());
+      assertEquals(-1, game.getWinner());
+      assertEquals(null, game.getLastWinningLine());
     }
 
     @Test
-    public void testGame() {
-      System.out.println("HELLO WORLD");
-      int x = 5;
-      assertEquals(5, x);
+    public void testMakeMove() {
+      game.makeMove(0, 0);
+      assertEquals(2, game.getCurrentPlayer());
+      assertEquals(1, game.getGameGrid()[0][0]);
+      assertEquals(0, game.getGameGrid()[1][1]);
+      assertEquals(0, game.getGameGrid()[2][2]);
+      assertEquals(false, game.getGameOver());
+      assertEquals(-1, game.getWinner());
+      assertEquals(null, game.getLastWinningLine());
+    }
+
+    @Test
+    public void testHorizontalWinRow1() {
+      // Winning line should be [0, 0] [0, 1] [0, 2]
+
+      game.makeMove(0, 0);
+      game.makeMove(1, 0);
+      game.makeMove(0, 1);
+      game.makeMove(1, 1);
+      game.makeMove(0, 2);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(0, game.getLastWinningLine().get(0)[1]);
+      assertEquals(0, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(0, game.getLastWinningLine().get(2)[0]);
+      assertEquals(2, game.getLastWinningLine().get(2)[1]);
+    }
+
+    @Test
+    public void testHorizontalWinRow2() {
+      // Winning line should be [1, 0] [1, 1] [1, 2]
+
+      game.makeMove(1, 0);
+      game.makeMove(0, 0);
+      game.makeMove(1, 1);
+      game.makeMove(0, 1);
+      game.makeMove(1, 2);
+      
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(1, game.getLastWinningLine().get(0)[0]);
+      assertEquals(0, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(1, game.getLastWinningLine().get(2)[0]);
+      assertEquals(2, game.getLastWinningLine().get(2)[1]);
+    }
+
+    @Test
+    public void testHorizontalWinRow3() {
+      // Winning line should be [2, 0] [2, 1] [2, 2]
+
+      game.makeMove(0, 0);
+      game.makeMove(2, 0);
+      game.makeMove(0, 1);
+      game.makeMove(2, 1);
+      game.makeMove(1, 2);
+      game.makeMove(2, 2);
+
+      assertEquals(2, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(2, game.getLastWinningLine().get(0)[0]);
+      assertEquals(0, game.getLastWinningLine().get(0)[1]);
+      assertEquals(2, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(2, game.getLastWinningLine().get(2)[1]);
+    }
+      
+    @Test
+    public void testVerticalWinCol1() {
+      // Winning line should be [0,0] [1, 0] [2, 0]
+      game.makeMove(0, 0);
+      game.makeMove(0, 1);
+      game.makeMove(1, 0);
+      game.makeMove(0, 2);
+      game.makeMove(2, 0);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(0, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(0, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(0, game.getLastWinningLine().get(2)[1]);
 
     }
+
+    @Test
+    public void testVerticalWinCol2() {
+      // Winning line should be [0,1] [1, 1] [2, 1]
+      game.makeMove(0, 1);
+      game.makeMove(0, 0);
+      game.makeMove(1, 1);
+      game.makeMove(0, 2);
+      game.makeMove(2, 1);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(1, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(1, game.getLastWinningLine().get(2)[1]);
+      
+    }
+
+    @Test
+    public void testVerticalWinCol3() {
+      // Winning line should be [0,2] [1, 2] [2, 2]
+      game.makeMove(0, 2);
+      game.makeMove(0, 0);
+      game.makeMove(1, 2);
+      game.makeMove(0, 1);
+      game.makeMove(2, 2);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(2, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(2, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(2, game.getLastWinningLine().get(2)[1]);
+
+    }
+
+    @Test
+    public void testLeftDiagonalWin() {
+      // Winning line should be [0,0] [1, 1] [2, 2]
+      game.makeMove(0, 0);
+      game.makeMove(0, 1);
+      game.makeMove(1, 1);
+      game.makeMove(0, 2);
+      game.makeMove(2, 2);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(0, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(2, game.getLastWinningLine().get(2)[1]);
+    }
+
+    @Test
+    public void testRightDiagonalWin() {
+      // Winning line should be [0,2] [1, 1] [2, 0]
+      game.makeMove(0, 2);
+      game.makeMove(0, 1);
+      game.makeMove(1, 1);
+      game.makeMove(0, 0);
+      game.makeMove(2, 0);
+
+      assertEquals(1, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(0, game.getLastWinningLine().get(0)[0]);
+      assertEquals(2, game.getLastWinningLine().get(0)[1]);
+      assertEquals(1, game.getLastWinningLine().get(1)[0]);
+      assertEquals(1, game.getLastWinningLine().get(1)[1]);
+      assertEquals(2, game.getLastWinningLine().get(2)[0]);
+      assertEquals(0, game.getLastWinningLine().get(2)[1]);
+    }
+  
+    @Test
+    public void testTie() {
+      // Winning line should be null
+      game.makeMove(0, 0);
+      game.makeMove(0, 1);
+      game.makeMove(0, 2);
+      game.makeMove(1, 0);
+      game.makeMove(1, 2);
+      game.makeMove(1, 1);
+      game.makeMove(2, 0);
+      game.makeMove(2, 2);
+      game.makeMove(2, 1);
+
+      assertEquals(0, game.getWinner());
+      assertEquals(true, game.getGameOver());
+      assertEquals(null, game.getLastWinningLine());
+    }
+  
+    @Test
+    public void testChangePlayer() {
+      assertEquals(1, game.getCurrentPlayer());
+      game.makeMove(0, 0);
+      assertEquals(2, game.getCurrentPlayer());
+      game.makeMove(0, 1);
+      assertEquals(1, game.getCurrentPlayer());
+
+    }
+  
 }
