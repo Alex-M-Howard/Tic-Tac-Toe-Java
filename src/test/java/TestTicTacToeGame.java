bc033045fccac4063459127f5dba0ea70af940df
Alex Howard <alexamh@zoho.com>
2023-05-14 21:28:20 -0400

    Fixed Junit dependency for testing

--- src/test/java/TestTicTacToeGame.java
+++ src/test/java/TestTicTacToeGame.java
@@ -1,26 +1,25 @@
-// package test.java;
-// import org.junit.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.alexhoward.tictactoe.TicTacToeGame;
 
-// import main.java.com.alexhoward.tictactoe.TicTacToeGame;
+public class TestTicTacToeGame {
+    TicTacToeGame game = new TicTacToeGame();
 
-// public class TestTicTacToeGame {
-//     TicTacToeGame game = new TicTacToeGame();
+    @BeforeAll
+    public static void setUp() {
+      System.out.println("BeforeClass");
+    }
 
-//     @BeforeClass
-//     public static void setUp() {
-//       System.out.println("BeforeClass");
-//     }
+    @AfterAll
+    public static void tearDown() {
+      System.out.println("AfterClass");
+    }
 
-//     @AfterClass
-//     public static void tearDown() {
-//       System.out.println("AfterClass");
-//     }
+    @Test
+    public void testGame() {
+      System.out.println("HELLO WORLD");
+      int x = 5;
+      assertEquals(5, x);
 
-//     @Test
-//     public void testGame() {
-//       System.out.println("HELLO WORLD");
-//       int x = 5;
-//       Assert.assertEquals(5, x);
-
-//     }
-// }
+    }
+}
